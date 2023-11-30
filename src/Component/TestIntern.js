2752cf2f7164aca83e990cd44dc8f645f5fcec43
bui cong anh <buiconganh641176@gmail.com>
2023-11-30 14:21:20 +0700

    create project and install libraries

---
+++ src/Component/TestIntern.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+
+export default function TestIntern() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
